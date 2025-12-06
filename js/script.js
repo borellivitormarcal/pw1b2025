2240b9d089c2ec894702964ed525a01f83b67ac6
Aluno.Etec <aula@etec.com>
2025-12-06 02:07:04 -0200

    pokedex 06/12

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch('https://pokeapi.co/api/v2/${pokemon}')
-
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/${pokemon}`)
+    
     const data = await APIResponse.json();
-
-    console.log(data);
+    return data;
 }
 
-fetchPokemon('25');
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+    ['animated']['front_default']
+}
+
+renderPokemon('25')
